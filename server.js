42caa035084132529fc03b0641c7ab17fe216bb1
Keiner K <kkenton@mmm.com>
2019-01-22 11:04:49 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-var fs = require("fs");
 var express = require("express");
-
 var app = express();
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.use(express.static(__dirname + ""));
 
 app.get("/", function(request, response){
-    response.send("hello!");
+    response.render('index.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;
